573ba1f4d335c84d3f2aeefa85a276cf84aa3870
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-05-09 16:48:41 +0200

    Create connexion.php

---
+++ Tps/tp1/connexion.php
@@ -0,0 +1,52 @@
+<?php 
+
+// Hachage du mot de passe
+
+$pass_hache = sha1($_POST['pass']);
+
+
+// Vérification des identifiants
+
+$req = $bdd->prepare('SELECT id FROM membres WHERE pseudo = :pseudo AND pass = :pass');
+
+$req->execute(array(
+
+    'pseudo' => $pseudo,
+
+    'pass' => $pass_hache));
+
+
+$resultat = $req->fetch();
+
+
+if (!$resultat)
+
+{
+
+    echo 'Mauvais identifiant ou mot de passe !';
+
+}
+
+else
+
+{
+
+    session_start();
+
+    $_SESSION['id'] = $resultat['id'];
+
+    $_SESSION['pseudo'] = $pseudo;
+
+    echo 'Vous êtes connecté !';
+
+}
+
+<?php 
+
+if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+
+{
+
+    echo 'Bonjour ' . $_SESSION['pseudo'];
+
+}
